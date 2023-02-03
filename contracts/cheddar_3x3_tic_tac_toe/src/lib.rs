1da6a86b6dc248eda805436db020f90e60f8dcd2
sczembor <43810037+sczembor@users.noreply.github.com>
2023-02-03 17:13:33 +0100

    Update contracts/cheddar_3x3_tic_tac_toe/src/lib.rs
    Co-authored-by: Robert Zaremba <robert@zaremba.ch>

--- contracts/cheddar_3x3_tic_tac_toe/src/lib.rs
+++ contracts/cheddar_3x3_tic_tac_toe/src/lib.rs
@@ -472,7 +472,7 @@ impl Contract {
 
     pub fn claim_timeout_win(&mut self, game_id: &GameId) {
         let mut game: Game = self.internal_get_game(&game_id);
-        let player = env::predecessor_account_id().clone();
+        let player = env::predecessor_account_id();
         if game.claim_timeout_win(player.clone()) == false {
             log!("can't claim the win, timeout didn't pass");
             return;
